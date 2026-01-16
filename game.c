e9cd2fed8b0a5da89d74bac56f98cdd21c6c8c94
jsanchez-codes <julian.sanchez@bsu.edu>
2026-01-16 23:36:39 +0000

    Working number guessing game. That lets the user know whether their guess is too high, too low, or correct

--- game.c
+++ game.c
@@ -2,20 +2,40 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define FALSE 0
+#define TRUE -1
+
 int main(){
-	char name[20];
-	printf("Hello. What is your name? ");
-	scanf("%s", name);
-	printf("Nice to meet you %s! Let's play a game. \n", name);
+	char userName[20];
+	int guess;
+	int correct;
+	int turns = 0;
+	int keepGoing = TRUE;
 
 	srand(time(NULL));
-	int a;
-	a = rand();
-	a = (a % 100) + 1;
-	printf("%d \n", a);
-	
-	printf("Please guess a number: ");
-	scanf("%d", &a);
+	correct = (rand() % 100) + 1;
+
+	printf("Correct: %d \n", correct);
+
+	printf("Hello. What is your name? ");
+	scanf("%s", userName);
+	printf("Nice to meet you %s. Let's play a game. \n", userName);
+
+	while (keepGoing == TRUE){
+		turns++;
+		printf("Please guess a number 1-100: ");
+		scanf("%d", &guess);
+		printf("You guessed %d. \n", guess);
+
+		if (guess < correct){
+			printf("Too low. \n");
+		}	else if (guess > correct){
+			printf("Too high. \n");
+		}	else {
+			printf("You guessed the number! \n");
+			keepGoing = FALSE;
+		} // end if
+	} // end while
 	
 	return 0;
 } // end main	
