55823ec75da1049b8bb9ce4ab519b5a4cc97f4ee
jsanchez-codes <julian.sanchez@bsu.edu>
2026-01-16 06:09:26 +0000

    Created code that greets the user and ask for their name and stores it in a string

---
+++ game.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+	char name[20];
+	printf("Hello. What is your name? ");
+	scanf("%s", name);
+	printf("Nice to meet you %s! Let's play a game. \n", name);
+
+	return 0;
+} // end main	
